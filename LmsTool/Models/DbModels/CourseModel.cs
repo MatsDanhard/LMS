406821f13c58a201a0c424c003cddc8922ab571d
Johan Hellman <kroyza@hotmail.com>
2017-07-17 10:17:14 +0200

    Uppdaterat index och Text

--- LmsTool/Models/DbModels/CourseModel.cs
+++ LmsTool/Models/DbModels/CourseModel.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace LmsTool.Models.DbModels
 {
     public class CourseModel
     {
         public int Id { get; set; }
-        [Display(Name = "Kursnamn")]
+        [Display(Name = "Kursens namn")]
         public string Name { get; set; }
+        [Display(Name = "Beskrivning")]
         public string Description { get; set; }
+        [Display(Name = "Startdatum")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/mm/yyyy}")]
         [DataType(DataType.Date)]
         public DateTime StartDate { get; set; } = DateTime.Now;
