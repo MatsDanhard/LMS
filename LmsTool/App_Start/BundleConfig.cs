452609a8284ad4b1395bf41cab1e317e6b5eff99
Johan Hellman <kroyza@hotmail.com>
2017-07-18 14:24:18 +0200

    Skapat jquery unobtrusive bundle, små layoutfix och css fix

--- LmsTool/App_Start/BundleConfig.cs
+++ LmsTool/App_Start/BundleConfig.cs
@@ -12,19 +12,27 @@ namespace LmsTool
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate.*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new StyleBundle("~/Content/modals").Include(
+                        "~/Content/bootstrap.min.css",
+                        "~/Content/bootstrap - theme.min.css"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/jquery-ui.css",
-                "~/Content/jquery-ui.css"
-                ));
-            bundles.Add(new ScriptBundle("~/Scripts/jqueryUI").Include(
+            bundles.Add(new ScriptBundle("~/Scripts/modals").Include(
+                        "~/Scripts/jquery-1.10.2.min.js",
+                        "~/Scripts/bootstrap.min.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/jQueryUI-css").Include(
+                        "~/Content/jquery-ui.css",
+                        "~/Content/jquery-ui.css"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/jQueryUI").Include(
                         "~/Scripts/jquery-ui.min.js",
-                        "~/Scripts/jquery-ui.js"
-                        ));
+                        "~/Scripts/jquery-ui.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
@@ -33,12 +41,10 @@ namespace LmsTool
                       "~/Scripts/respond.js", 
                       "~/Content/index.js",
                       "~/Scripts/jquery-1.10.2.*"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"
-                      ));
-
-
+                      "~/Content/site.css"));
         }
     }
 }
