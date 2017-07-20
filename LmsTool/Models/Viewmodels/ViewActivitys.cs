c89308bc80d4886d64910cce5795df265dd01118
Johan Hellman <kroyza@hotmail.com>
2017-07-20 13:16:52 +0200

    Översättning knappar och script + css updateringar

--- LmsTool/Models/Viewmodels/ViewActivitys.cs
+++ LmsTool/Models/Viewmodels/ViewActivitys.cs
@@ -12,15 +12,21 @@ namespace LmsTool.Models.Viewmodels
     public class ViewActivitys
     {
         public int Id { get; set; }
-        public string TypeOfActivity { get; set; } 
+        [Display(Name="Aktivitetstyp")]
+        public string TypeOfActivity { get; set; }
+        [Display(Name = "Aktivitet")]
         public string Name { get; set; }
+        [Display(Name = "Beskrivning")]
         public string Description { get; set; }
+        [Display(Name = "Modulens namn")]
         public string ModulName { get; set; }
+        [Display(Name = "Modulens startdatum")]
         public string ModulStartStr { get; set; }
+        [Display(Name = "Modulens slutdatum")]
         public string ModulEndStr { get; set; } 
-        [Display(Name = "Startdatum")]
+        [Display(Name = "Starttid")]
         public DateTime StartDate { get; set; } = DateTime.Now;
-        [Display(Name = "Slutdatum")]
+        [Display(Name = "Sluttid")]
         public DateTime EndDate { get; set; } = DateTime.Now.AddDays(1);
         [HiddenInput]
         public int ModulId { get; set; }
